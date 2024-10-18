02abd2b43d96a088f7b24fb6c406b8a9674fe1a7
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-18 16:03:53 +0200

    Added a button to test the website
    Co-authored-by: Huan <bentonaw@users.noreply.github.com>

---
+++ BladReact2/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function MainPage() {
+
+  return (
+		<>
+			<Button>Button</Button>
+			<Button variant='pill'>Button</Button>
+		</>
+	);
+}
