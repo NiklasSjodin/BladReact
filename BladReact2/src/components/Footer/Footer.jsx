3ac2a2992f329d827ee333f5fc908b81e4f6e4cf
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-18 16:04:42 +0200

    Added footer, layout and header components
    Co-authored-by: Huan <bentonaw@users.noreply.github.com>

---
+++ BladReact2/src/components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+	return (
+		<footer className="w-full border-t-2 border-black pt-4">
+			<nav className="container mx-auto flex justify-between items-center">
+				<p>&copy; 2024 blad.</p>
+				<ul className="flex space-x-4">
+					<li>
+						<Link
+							to=""
+							target="_blank"
+							rel="noopener noreferrer"
+							title="link to github"
+						>
+							<i className="fa-brands fa-github"></i>
+						</Link>
+					</li>
+					<li>
+						<Link
+							to=""
+							target="_blank"
+							rel="noopener noreferrer"
+							title="link to linkedin"
+						>
+							<i className="fa-brands fa-linkedin"></i>
+						</Link>
+					</li>
+					<li>
+						<Link
+							to=""
+							target="_blank"
+							rel="noopener noreferrer"
+							title="link to instagram"
+						>
+							<i className="fa-brands fa-instagram"></i>
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ BladReact2/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+export default function Header() {
+
+
+	return (
+		<header className='border-t-2 border-black pt-4'>
+			test
+		</header>
+	);
+}
