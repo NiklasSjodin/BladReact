5fcdceb2d595a78ec9aa2ba99c3680d046014211
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-22 11:13:33 +0200

    Added imports and components

--- BladReact2/src/pages/MainPage/MainPage.jsx
+++ BladReact2/src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,28 @@
 import { Button } from '@/components/ui/button';
+import { MBButton } from '@/components/ui/moving-border';
+import { TextRevealCard,TextRevealCardTitle,TextRevealCardDescription } from '@/components/ui/text-reveal-card';
 
 export default function MainPage() {
 
   return (
 		<>
-			<Button>Button</Button>
+		<Button>Button</Button>
 			<Button variant='pill'>Button</Button>
+			<MBButton>Moving Border Button</MBButton>
+			<div className='flex items-center justify-center bg-[#0E0E10] h-[40rem] rounded-2xl w-full'>
+				<TextRevealCard
+					text='You know the business'
+					revealText='I know the chemistry '
+				>
+					<TextRevealCardTitle>
+						Sometimes, you just need to see it.
+					</TextRevealCardTitle>
+					<TextRevealCardDescription>
+						This is a text reveal card. Hover over the card to reveal the hidden
+						text.
+					</TextRevealCardDescription>
+				</TextRevealCard>
+			</div>
 		</>
 	);
 }
