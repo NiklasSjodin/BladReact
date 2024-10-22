eaf4b0137c01f7b88ad2e7842da874c826cc47a0
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-22 11:26:02 +0200

    Added signup and login pages

--- BladReact2/src/App.jsx
+++ BladReact2/src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import MainPage from './pages/MainPage/MainPage';
-
+import LogInPage from './pages/LogInPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Layout />}>
 					<Route index element={<MainPage />} />
+					<Route path="login" element={<LogInPage />} />
+					<Route path="signup" element={<SignUpPage />} />
 				</Route>
 			</Routes>
 		</Router>
