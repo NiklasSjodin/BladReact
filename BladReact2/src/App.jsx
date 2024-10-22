eafe3b7182ca026a3d7b7ac73bca446012eec095
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-22 11:13:17 +0200

    Added imports and components

--- BladReact2/src/App.jsx
+++ BladReact2/src/App.jsx
@@ -1,8 +1,4 @@
 import './App.css'
-
-import { Button } from '@/components/ui/button';
-import { MBButton } from '@/components/ui/moving-border';
-import { TextRevealCard,TextRevealCardTitle,TextRevealCardDescription } from './components/ui/text-reveal-card';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import MainPage from './pages/MainPage/MainPage';
@@ -12,25 +8,6 @@ function App() {
 
   return (
 		<>
-			<Button>Button</Button>
-			<Button variant='pill'>Button</Button>
-			<MBButton>Moving Border Button</MBButton>
-			<div className='flex items-center justify-center bg-[#0E0E10] h-[40rem] rounded-2xl w-full'>
-				<TextRevealCard
-					text='You know the business'
-					revealText='I know the chemistry '
-				>
-					<TextRevealCardTitle>
-						Sometimes, you just need to see it.
-					</TextRevealCardTitle>
-					<TextRevealCardDescription>
-						This is a text reveal card. Hover over the card to reveal the hidden
-						text.
-					</TextRevealCardDescription>
-				</TextRevealCard>
-			</div>
-		</>
-
 		<Router>
 			<Routes>
 				<Route path='/' element={<Layout />}>
@@ -38,7 +15,7 @@ function App() {
 				</Route>
 			</Routes>
 		</Router>
-
+		</>
 	);
 }
 
