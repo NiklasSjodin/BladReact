d43cb48ef45f2f99cf5444e385b881f65b40e745
Huan <98620169+bentonaw@users.noreply.github.com>
2024-10-25 14:15:49 +0200

    fix layout

--- BladReact2/src/components/layout/Layout.jsx
+++ BladReact2/src/components/layout/Layout.jsx
@@ -1,15 +1,44 @@
-import { Outlet } from 'react-router-dom';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
+import { Outlet, useLocation } from 'react-router-dom';
+import Footer from '../Footer/DesktopFooter';
+import Header from '../Header/DesktopHeader';
+import MobileHeader from '../Header/MobileHeader';
+import MainPageHeader from '../Header/MainPageHeader';
+import MobileNavbar from '../Navbar/MobileNavbar';
 
 const Layout = () => {
+
+	const location = useLocation();
+
+
 	return (
 		<div className='flex flex-col h-screen overflow-hidden'>
-			<Header />
+			{location.pathname === '/login' ? (
+				<></>
+			) : (
+				<>
+					{/* <div className='block md:hidden sm:hidden'>
+						<MobileHeader />
+					</div> */}
+					<div className='hidden md:block'>
+						<Header />
+					</div>
+				</>
+			)}
 			<main className='flex-grow overflow-auto'>
 				<Outlet />
 			</main>
-			<Footer />
+			{location.pathname === '/login' ? (
+				<></>
+			) : (
+				<>
+					<div className='block lg:hidden'>
+						<MobileNavbar />
+					</div>
+					<div className='hidden md:block'>
+						<Footer />
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
