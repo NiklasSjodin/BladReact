b983626e15bd86ef5d4ff986af53131870c5d3bd
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-25 14:12:57 +0200

    Added links to sign up and log in page

--- BladReact2/src/components/Header/Header.jsx
+++ BladReact2/src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
+import {Link} from 'react-router-dom'
 
 export default function Header() {
 
 
 	return (
-		<header className='border-t-2 border-black pt-4'>
-			test
+		<header className='border-b-2 border-black pt-4'>
+			<Link to="login">Login</Link>
+			<Link to="signup">Sign Up</Link>
 		</header>
 	);
 }
