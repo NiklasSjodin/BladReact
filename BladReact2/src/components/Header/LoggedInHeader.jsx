8c0b101423ae0fe46c50db3cc5df59e08c203eb9
Huan <98620169+bentonaw@users.noreply.github.com>
2024-11-05 08:44:23 +0100

    add loggedin header

---
+++ BladReact2/src/components/Header/LoggedInHeader.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { Button } from '../ui/button';
+import { Link } from 'react-router-dom';
+import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar';
+
+export default function LoggedInHeader() {
+	return (
+		<header className='border-b-2 border-black pt-1 pb-1'>
+			<div className='container px-4 h-12 flex items-center justify-between'>
+				<div className='flex-1 text-center'>
+					<span>blad.</span>
+				</div>
+				<div className='flex items-center space-x-4 ml-auto px-2'>
+					<Link to='/clubs'>Clubs</Link>
+					<Link to='/explore'>Explore</Link>
+					<Link to='/bibliotek'>Bibliotek</Link>
+				</div>
+
+				<div className='flex items-center space-x-4 ml-auto pl-2'>
+					<input
+						type='text'
+						placeholder='Search...'
+						className='border rounded-md px-2 py-1'
+					/>
+					<Avatar>
+						<AvatarImage src='/path/to/avatar.jpg' alt='User Avatar' />
+						<AvatarFallback>U</AvatarFallback>
+					</Avatar>
+					<span>Username</span>
+				</div>
+			</div>
+		</header>
+	);
+}
