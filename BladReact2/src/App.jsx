35955dd41f5b05a07386ea61c3cc1c649cd2c3da
Niklas Sjödin <sjoodiin@hotmail.com>
2024-11-06 14:22:12 +0100

    Added components for faq page, privacy page, contact us page, termsofservice page and the matching routes

--- BladReact2/src/App.jsx
+++ BladReact2/src/App.jsx
@@ -4,9 +4,16 @@ import Layout from './components/layout/Layout';
 import MainPage from './pages/MainPage/MainPage';
 import LogInPage from './pages/LogInPage';
 import SignUpPage from './pages/SignUpPage';
+import FAQPage from './pages/FAQpage';
+import PrivacyPolicy from './pages/PrivacyPage';
+import ContactUs from './pages/ContactUsPage';
+import TermsOfService from './pages/TermsOfServicePage';
 import Home from './pages/LoggedIn/Home';
 import ProtectedRoute from './components/layout/ProtectedRoute';
 import Library from './pages/LoggedIn/Library';
+import UserProfile from './pages/LoggedIn/UserProfile';
+import UserProfileSettings from './pages/LoggedIn/UserProfileEditSettings';
+import Support from './pages/LoggedIn/Support';
 
 const App = () => {
 	return (
@@ -17,6 +24,13 @@ const App = () => {
 						<Route index element={<MainPage />} />
 						<Route path='login' element={<LogInPage />} />
 						<Route path='signup' element={<SignUpPage />} />
+						<Route path='faq' element={<FAQPage />} />
+						<Route path='privacy' element={<PrivacyPolicy />} />
+						<Route path='termsofservice' element={<TermsOfService />} />
+						<Route path='contact' element={<ContactUs />} />
+						<Route path='account' element={<UserProfile />} />
+						<Route path='settings' element={<UserProfileSettings />} />
+						<Route path='support' element={<Support />} />
 						{/* Skyddad rutt för Home-sidan */}
 						<Route
 							path='home'
