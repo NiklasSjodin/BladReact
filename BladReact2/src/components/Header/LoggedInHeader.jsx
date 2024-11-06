ee7287912208b9cdd6a38066af6b5219f43ed146
Huan <98620169+bentonaw@users.noreply.github.com>
2024-11-06 09:40:50 +0100

    add library placeholder

--- BladReact2/src/components/Header/LoggedInHeader.jsx
+++ BladReact2/src/components/Header/LoggedInHeader.jsx
@@ -8,12 +8,14 @@ export default function LoggedInHeader() {
 		<header className='border-b-2 border-black pt-1 pb-1'>
 			<div className='container px-4 h-12 flex items-center justify-between'>
 				<div className='flex-1 text-center'>
-					<span>blad.</span>
+					<Link to='/home'>
+						<span>blad.</span>
+					</Link>
 				</div>
 				<div className='flex items-center space-x-4 ml-auto px-2'>
 					<Link to='/clubs'>Clubs</Link>
 					<Link to='/explore'>Explore</Link>
-					<Link to='/bibliotek'>Bibliotek</Link>
+					<Link to='/library'>Bibliotek</Link>
 				</div>
 
 				<div className='flex items-center space-x-4 ml-auto pl-2'>
--- BladReact2/src/pages/LoggedIn/Library.jsx
+++ BladReact2/src/pages/LoggedIn/Library.jsx
@@ -1,7 +1,7 @@
 import Footer from '@/components/Footer/DesktopFooter';
 import { Link } from 'react-router-dom';
 
-export default function Home() {
+export default function Library() {
 	return (
 		<>
 			<div className='flex flex-col min-h-screen'>
