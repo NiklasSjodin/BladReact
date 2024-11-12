09930509404a7945c8b755870270506040c3d8a1
Huan <98620169+bentonaw@users.noreply.github.com>
2024-11-12 02:20:58 +0100

    refactor header design

--- BladReact2/src/components/Header/DesktopHeader.jsx
+++ BladReact2/src/components/Header/DesktopHeader.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import {Button} from '../ui/button'
 import { Link } from 'react-router-dom';
+import logo from "../../images/books.png";
 
 export default function Header() {
-
-
 	return (
-		<header className='border-b-2 border-black pt-1 pb-1'>
-			<div className='container px-4 h-12 flex items-center justify-between'>
-				<div className='ml-auto'>
-					blad.
-				</div>
-				<div className='ml-auto'>
+		<header className='pt-1 pb-1 bg-slate-600'>
+			<div className='px-4 h-12 flex items-center'>
+				<img
+					src={logo}
+					alt='Description of image'
+					className='h-8 w-auto object-contain pr-1'
+				/>
+				<div className='flex-1 font-comico'>blad.</div>
+				
+				<div>
 					<Button variant='outline'>
 						<Link to='/login'>Login</Link>
 					</Button>
--- BladReact2/src/components/Header/LoggedInHeader.jsx
+++ BladReact2/src/components/Header/LoggedInHeader.jsx
@@ -2,16 +2,19 @@ import { useEffect, useState } from 'react';
 import { Button } from '../ui/button';
 import { Link } from 'react-router-dom';
 import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar';
+import logo from '../../images/books.png';
 
 export default function LoggedInHeader() {
 	return (
-		<header className='border-b-2 border-black pt-1 pb-1'>
-			<div className='container px-4 h-12 flex items-center justify-between'>
-				<div className='flex-1 text-center'>
-					<Link to='/home'>
-						<span>blad.</span>
-					</Link>
-				</div>
+		<header className='pt-1 pb-1 bg-slate-600'>
+			<div className='px-4 h-12 flex items-center'>
+				<img
+					src={logo}
+					alt='Description of image'
+					className='h-8 w-auto object-contain pr-1'
+				/>
+				<div className='flex-1 font-comico'>blad.</div>
+				
 				<div className='flex items-center space-x-4 ml-auto px-2'>
 					<Link to='/clubs'>Clubs</Link>
 					<Link to='/explore'>Explore</Link>
@@ -21,7 +24,7 @@ export default function LoggedInHeader() {
 				<div className='flex items-center space-x-4 ml-auto pl-2'>
 					<input
 						type='text'
-						placeholder='Search...'
+						placeholder='Sökbar för allt?'
 						className='border rounded-md px-2 py-1'
 					/>
 					<Avatar>
