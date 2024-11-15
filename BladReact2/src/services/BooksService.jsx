39075058048cc912a8713acd2fe742ba86b65bba
Huan <98620169+bentonaw@users.noreply.github.com>
2024-11-15 12:15:23 +0100

    refactor booksservice calls backend

--- BladReact2/src/services/BooksService.jsx
+++ BladReact2/src/services/BooksService.jsx
@@ -1,15 +1,18 @@
-// services/booksService.js
-export const fetchTrendingBooks = async () => {
+const API_URL = 'https://localhost:7076/';
+
+export const FetchBooks = async (searchQuery = '') => {
 	try {
 		const response = await fetch(
-			'https://openlibrary.org/search.json?q=javascript&limit=10'
+			`${API_URL}api/OpenLibraryAPI/search?query=${searchQuery}`
 		);
 		const data = await response.json();
 		return data.docs.map((book) => ({
 			id: book.key,
 			title: book.title,
 			author: book.author_name ? book.author_name[0] : 'Unknown',
-			coverId: book.cover_i,
+			coverId: book.cover_i
+				? `${API_URL}api/OpenLibraryAPI/cover/${book.cover_i}`
+				: null,
 		}));
 	} catch (error) {
 		console.error('Error fetching books:', error);
