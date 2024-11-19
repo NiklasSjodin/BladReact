a4cab59669575a81b0a7deec5c614ec6c39b50cc
Niklas Sjödin <sjoodiin@hotmail.com>
2024-11-19 16:21:39 +0100

    Changed the color of the background

--- BladReact2/src/components/layout/PageContainer.jsx
+++ BladReact2/src/components/layout/PageContainer.jsx
@@ -1,5 +1,5 @@
 export const PageContainer = ({ children }) => (
-	<div className='bg-gradient-to-br from-gray-500 to-black min-h-screen'>
+	<div className='bg-bladLightBackground min-h-screen'>
 		<div className='flex flex-col items-center'>
 			<main className='w-full max-w-screen-lg px-4'>{children}</main>
 		</div>
