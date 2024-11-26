ab5516df845db4e7a735b11d2b307f5516eda3de
Huan <98620169+bentonaw@users.noreply.github.com>
2024-11-26 18:39:11 +0100

    add: jwtdecode to home

--- BladReact2/src/pages/LoggedIn/Home/Home.jsx
+++ BladReact2/src/pages/LoggedIn/Home/Home.jsx
@@ -10,6 +10,7 @@ import { SectionHeader } from '../../../components/Sections/SectionHeader';
 import { fetchBooks } from '../../../services/BooksService';
 import ScrollableContainer from '../../../components/Sections/ScrollableContainer';
 import HeroSection from '../../../components/Sections/HeroSection';
+import { jwtDecode } from 'jwt-decode';
 
 /**
  * Main Home Component
@@ -23,6 +24,7 @@ import HeroSection from '../../../components/Sections/HeroSection';
 export default function Home() {
 	const [popularBooks, setPopularBooks] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [userName, setUserName] = useState('');
 
 	useEffect(() => {
 		const loadBooks = async () => {
@@ -43,13 +45,23 @@ export default function Home() {
 		loadBooks();
 	}, []);
 
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		if (token) {
+			const decoded = jwtDecode(token);
+			setUserName(
+				decoded['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name']
+			);
+		}
+	}, []);
+
 	return (
 		<PageContainer>
 			<HeroSection />
 			<div className='space-y-8 pb-24 pt-16'>
 				<div className='pt-6 space-y-4'>
 					<h1 className='text-3xl font-bold text-bladLightTextColor'>
-						Welcome to Your Library
+						Welcome {userName}
 					</h1>
 					<p className='text-gray-400'>Discover your next favorite book</p>
 				</div>
@@ -65,7 +77,6 @@ export default function Home() {
 						  ))
 						: popularBooks.map((book) => <BookCard key={book.id} {...book} />)}
 				</ScrollableContainer>
-
 			</div>
 		</PageContainer>
 	);
